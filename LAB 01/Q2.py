0ee382ac7e9ab9e83379432b16b81cb070a1a010
Simal Hassan <simal.mpgss@gmail.com>
2026-01-26 22:39:38 +0500

    Create Q2.py

---
+++ LAB 01/Q2.py
@@ -0,0 +1,11 @@
+#Q2
+nnum = int(input("Enter nth number: "))
+i = 0
+count = 0
+print("Even numbers: ")
+while i != nnum:
+    print(" ",i," ")
+    i = i+2
+    count = count +1
+
+print("\nTotal even numbers: ", count)
