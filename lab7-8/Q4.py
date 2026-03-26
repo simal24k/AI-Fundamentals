e68f60643be9ccbcad4cdfd0d9736b956aa71576
Simal Hassan <simal.mpgss@gmail.com>
2026-03-26 23:48:24 +0500

    Create Q4.py

---
+++ lab7-8/Q4.py
@@ -0,0 +1,25 @@
+from ortools.sat.python import cp_model
+ 
+print("\n" + "=" * 50)
+print("TASK 4 – Basic CSP")
+print("=" * 50)
+ 
+csp = cp_model.CpModel()
+ 
+p = csp.new_int_var(0, 3, "P") 
+q = csp.new_int_var(0, 3, "Q") 
+r = csp.new_int_var(0, 3, "R") 
+ 
+csp.add(p != q)        
+csp.add(q != r)        
+csp.add(p + q <= 4)    
+ 
+solver = cp_model.CpSolver()
+status = solver.solve(csp)
+ 
+if status in (cp_model.FEASIBLE, cp_model.OPTIMAL):
+    print(f"P = {solver.value(p)}")
+    print(f"Q = {solver.value(q)}")
+    print(f"R = {solver.value(r)}")
+else:
+    print("No solution found.")
